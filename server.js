0b37d27d2f1fe1822d7dfab51dba7000f5f1bd7a
boybenson <ybenson96@gmail.com>
2021-01-27 11:44:02 +0000

    added a profile

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import paystackRouters from "./routes/paystackRouters.js";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -9,5 +10,13 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 
 app.use("/api/payment", paystackRouters);
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname + "/frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
 app.listen(port, () => console.log(`App is running on port ${port}`));
